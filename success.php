94dd7f4e731b7ebbe8c59019d760ae67614c74e0
gurpreetdhindsa <gurprit449@gmail.com>
2021-07-03 20:26:49 +0530

    Initial commit

---
+++ success.php
@@ -0,0 +1,4 @@
+<title>Payment Successful - NaturalJuices</title>
+<meta name="description" content="Payment Successful">
+<center><b>Payment Successful</b></center>
+<center><a href="https://www.naturaljuices.co.uk/">return</a></center>
